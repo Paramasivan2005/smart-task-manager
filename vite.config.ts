7bc43a8fe0b869246d2ebd7cb7095f6ebd6feac7
Paramasivan2005 <sakthish2000@gmail.com>
2026-03-04 12:49:01 +0530

    updated vite config and dashboard

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        dashboard: resolve(__dirname, "dashboard.html"),
+      },
+    },
+  },
+});
